383fa549d0aea65f27cef8eab0670fcf1b195b3c
League <league@iMac-6.attlocal.net>
2019-05-05 15:56:46 -0700

    ArrayListDemo, GuestBook, IntroToArrayLists :smile:

--- src/photo_quiz/PhotoQuiz.java
+++ src/photo_quiz/PhotoQuiz.java
@@ -26,34 +26,50 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-
+		String url = "http://cdn.shopify.com/s/files/1/0004/8132/9204/products/hersheys-milk-chocolate-candy-bar_1_1024x1024.jpg?v=1522355784";
 		// 2. create a variable of type "Component" that will hold your image
-
+			Component component;
 		// 3. use the "createImage()" method below to initialize your Component
-
+				component = createImage(url);
 		// 4. add the image to the quiz window
-
+				quizWindow.add(component);
 		// 5. call the pack() method on the quiz window
-
+				quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+				String answer = JOptionPane.showInputDialog("What is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+				if(answer.equals("chocolate")) {
+					System.out.println("CORRECT");
+				}
 		// 8. print "INCORRECT" if the answer is wrong
-
+				else {
+					System.out.println("INCORRECT");
+					
+				}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+				quizWindow.remove(component);
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+				String newImage = "http://cdn.shopify.com/s/files/1/0004/8132/9204/products/bun-vanilla-bar_1_1024x1024.jpg?v=1522355587";
+				Component newComponent = createImage(newImage);
 		// 11. add the second image to the quiz window
+				quizWindow.add(newComponent);
 
 		// 12. pack the quiz window
-
+				quizWindow.pack();
 		// 13. ask another question
+				String anotherAnswer = JOptionPane.showInputDialog("Is this tasty?");
 
 		// 14+ check answer, say if correct or incorrect, etc.
+				if (anotherAnswer.equals("no")) {
+					System.out.println("CORRECT");
+					
+				}
+				else {
+					System.out.println("INCORRECT");
+				}
 
 	}
 
